34b9b15bb7c4920060e36ce013fbc7c44b35300f
Nikhil5858 <123rathodnikhil@gmail.com>
2025-01-22 17:29:26 +0530

    fcaulty crud

--- sem-2/WT-2/Lab-7/controller/facultyController.js
+++ sem-2/WT-2/Lab-7/controller/facultyController.js
@@ -23,4 +23,35 @@ const findFaculty = async(req,res)=>{
     }
 }
 
-module.exports = {addFaculty,findFaculty};
+const findFacultyById = async(req,res)=>{
+    const {id} = req.params 
+    try {
+        const findFaculty = await Faculty.findById(id)
+        res.status(200).json(findFaculty);
+    } catch (error) {
+        res.status(400).json({error:err.message})
+    }
+}
+
+const facultyDelete = async(req,res)=>{
+    const {id} = req.params;
+    try {
+        const deleteFaculty = await Faculty.findByIdAndDelete(id);
+        res.status(200).json(deleteFaculty) 
+    } catch (error) {
+        res.status(400).json({error:err.message})
+    }
+}
+
+const facultyUpdate = async(req,res)=>{
+    const {id} = req.params;
+    const data = req.body; 
+    try {
+        const updateFaculty = await Faculty.findByIdAndUpdate(id,data,{new:true})
+        res.status(200).json(updateFaculty);
+    } catch (error) {
+        res.status(400).json({error:err.message})
+    }
+}
+
+module.exports = {addFaculty,findFaculty,findFacultyById,facultyDelete,facultyUpdate};
--- sem-2/WT-2/Lab-7/routes/facultyRouter.js
+++ sem-2/WT-2/Lab-7/routes/facultyRouter.js
@@ -2,8 +2,12 @@ const express = require('express')
 const app = express();
 const router = express.Router()
 
-const {addFaculty,findFaculty} = require('../controller/facultyController')
+const {addFaculty,findFaculty,findFacultyById,facultyDelete,facultyUpdate} = require('../controller/facultyController')
 
 router.post('/create',addFaculty)
 router.get('/find',findFaculty)
+router.get('/find/:id',findFacultyById)
+router.get('/delete/:id',facultyDelete)
+router.post('/update/:id',facultyUpdate)
+
 module.exports = router
