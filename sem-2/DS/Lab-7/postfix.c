ac81d6c648cf3d5561eda2d91328d55d8f99a82f
Nikhil5858 <123rathodnikhil@gmail.com>
2025-01-25 18:31:03 +0530

    infix to postfix ds

---
+++ sem-2/DS/Lab-7/postfix.c
@@ -0,0 +1,96 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+
+int top = -1;
+char postfix[100];
+char s[100];
+
+char pop(){
+	if(top == -1){
+		printf("Stack Is UnderFlow!");
+		return '\0';
+	}
+	else{
+//		char x = s[top];
+//		s[top] = '\0';
+//		top--;
+		return s[top--];
+	}
+}
+
+void push(char ch){
+	if(top>=99){
+		printf("Stack is Overflow");
+	}
+	else{
+		top = top + 1;
+		s[top]=ch;
+	}
+}
+
+int oppre(char op){
+	switch(op){
+		case '+':
+		case '-':
+			return 1;
+			break;
+		case '*':
+		case '/':
+			return 2;
+			break;
+		case '^':
+			return 3;
+			break;
+		default:
+			return 0;
+	}
+}
+
+int main(){
+	char input[100];
+	int i=0;
+	int j=0;
+	printf("Enter Infix Expression : \n");
+	scanf("%s",input);
+	
+//	input[strlen(input)] = ')';
+	strcat(input,")");
+	
+	push('(');
+	
+	while(input[i] != '\0'){
+		char temp = input[i];
+		if(temp == '('){
+			push(temp);
+		}
+		else if(isalnum(temp)){
+			postfix[j] =temp;
+			j++;
+		}
+		else if(temp == ')'){
+			while(s[top] != '('){
+				postfix[j] = pop();
+				j++;
+			}
+			char x = pop();
+		}
+		else if(temp =='+' || temp == '-' || temp == '/'|| temp == '*' || temp == '^'){
+			while(oppre(s[top]) >= oppre(temp)){
+				postfix[j] = pop();
+				j++;
+			}
+			push(temp);
+		}
+		i++;
+	}
+	printf("%s",postfix);
+}
+
+
+
+
+
+
+
+
