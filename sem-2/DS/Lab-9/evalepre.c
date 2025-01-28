f1f32902ddbdbed2fbf751cd41d7bce5ec66152e
Nikhil5858 <123rathodnikhil@gmail.com>
2025-01-28 17:43:42 +0530

    lab-9

---
+++ sem-2/DS/Lab-9/evalepre.c
@@ -0,0 +1,89 @@
+#include<stdio.h>
+#include<ctype.h>
+
+int top = -1;
+char s[100];
+int op1,op2,value=0;
+
+void push(char ch){
+	if(top>=99){
+		printf("Stack Is Overflow!");
+	}
+	else{
+		top = top + 1;
+		s[top]= ch;
+	}
+}
+
+char pop(){
+	if(top == -1){
+		printf("Stack is Underflow!");
+		return '\0';
+	}
+	else{
+		char x = s[top];
+		s[top] = '\0';
+		top--;
+		return x;
+	}
+}
+
+int preop(char op){
+	switch(op){
+		case '+':
+			return op1+op2;
+			break;
+		case '-':
+			return op1-op2;
+			break;
+		case '*':
+			return op1*op2;
+			break;
+		case '/':
+			return op1/op2;
+			break;
+		default:
+			return 0;
+	}	
+}
+
+void main(){
+	char postfix[100];
+	printf("Enter Prefix Evulation : ");
+	scanf("%s",postfix);
+	printf("%s",strrev(postfix));
+	int i=0;
+	char temp;
+	while(postfix[i] != '\0'){
+		temp = postfix[i];
+		if(isdigit(temp)){
+			push(temp);
+		}
+		else{
+			char c = pop();
+			op2 = c - '0';
+			c = pop();
+			op1 = c - '0';
+			value = preop(temp);
+			push(value + '0');
+		}
+		i++;
+	}
+	printf("%d",pop()-'0');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
---
+++ sem-2/DS/Placement/extra3.java
@@ -0,0 +1,13 @@
+public class extra3 {
+
+    public static void main(String[] args) {
+        int num=1;
+        for (int i = 1; i < 5; i++) {
+            for (int j = 1; j < 5; j++) {
+                    System.out.print(num+"  ");
+                    num++;
+            }
+            System.out.println();
+        }
+    }
+}
